ab408dc0255d1976336ce173b88e44b004e736de
Elias <elias@Mac.fritz.box>
2026-03-31 20:56:02 +0200

    add new

--- api/app/Models/PushSubscription.php
+++ api/app/Models/PushSubscription.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use NotificationChannels\WebPush\PushSubscription as BasePushSubscription;
 
-class PushSubscription extends Model
+class PushSubscription extends BasePushSubscription
 {
-    protected $fillable = ['user_id', 'endpoint', 'endpoint_hash', 'public_key', 'auth_token', 'content_encoding'];
+    protected $fillable = [
+        'user_id',
+        'endpoint',
+        'endpoint_hash',
+        'public_key',
+        'auth_token',
+        'content_encoding'
+    ];
 
     protected static function booted()
     {
